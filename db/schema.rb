fdf60d799e067453ed5ebec48769f9941ed0acef
Michel <micheldascse01@gmail.com>
2020-02-10 11:56:42 +0900

    Completed many to many association and this might be the end of this app

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_075752) do
+ActiveRecord::Schema.define(version: 2020_02_10_012203) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
